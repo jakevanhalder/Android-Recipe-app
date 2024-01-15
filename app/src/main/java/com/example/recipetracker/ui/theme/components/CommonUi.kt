5843d4420a37d2099204c5279487950478de229a
jakevanhalder <124746901+jakevanhalder@users.noreply.github.com>
2024-01-15 13:07:23 -0800

    worked on the home screen and common ui

--- app/src/main/java/com/example/recipetracker/ui/theme/components/CommonUi.kt
+++ app/src/main/java/com/example/recipetracker/ui/theme/components/CommonUi.kt
@@ -2,10 +2,14 @@ package com.example.recipetracker.ui.theme.components
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -14,11 +18,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.fontResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.recipetracker.R
 
 @Composable
 fun TopBar(value: String){
@@ -33,11 +35,21 @@ fun TopBar(value: String){
                 contentDescription = "Menu"
             )
         }
+
         Text(
             text = value,
             color = Color.Black,
             fontSize = 20.sp
         )
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = "Search"
+            )
+        }
     }
 }
 
@@ -47,4 +59,42 @@ fun TopBarPreview(){
     TopBar("My Recipes")
 }
 
+@Composable
+fun BottomBar(){
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(
+                imageVector = Icons.Default.Home,
+                contentDescription = "Home"
+            )
+        }
 
+        Spacer(modifier = Modifier.weight(0.5f))
+
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(
+                imageVector = Icons.Default.AddCircle,
+                contentDescription = "Add Recipe"
+            )
+        }
+
+        Spacer(modifier = Modifier.weight(0.5f))
+
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(
+                imageVector = Icons.Default.Person,
+                contentDescription = "Profile"
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BottomBarPreview(){
+    BottomBar()
+}
--- app/src/main/java/com/example/recipetracker/ui/theme/home/HomeScreen.kt
+++ app/src/main/java/com/example/recipetracker/ui/theme/home/HomeScreen.kt
@@ -1,37 +1,59 @@
 package com.example.recipetracker.ui.theme.home
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.example.recipetracker.R
+import com.example.recipetracker.ui.theme.components.BottomBar
+import com.example.recipetracker.ui.theme.components.RecipeCard
 import com.example.recipetracker.ui.theme.components.TopBar
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(navController: NavHostController) {
-    Surface(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Column(
+
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize(),
+        topBar = {
+            TopBar(value = "My Recipes")
+        },
+        bottomBar = {
+            NavigationBar {
+                BottomBar()
+            }
+        }
+    ) { values ->
+        LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(18.dp)
-        ) {
-            TopBar("My Recipes")
-
-            /*items(100) {
+                .padding(values)
+        ){
+            items(100) {
                 RecipeCard(
                     painter = painterResource(id = R.drawable.hamburger_steak_4),
                     contentDescription = "Burger Steak Recipe",
                     title = "Burger Steak",
                     fontFamily = androidx.compose.ui.text.font.FontFamily.Default
                 )
-            }*/
+            }
         }
     }
 }
