a17b19a32128012246e5f79466fb4b9401c60b27
jakevanhalder <124746901+jakevanhalder@users.noreply.github.com>
2024-01-16 19:06:41 -0800

    made the app icon

--- app/src/main/java/com/example/recipetracker/ui/theme/home/HomeScreen.kt
+++ app/src/main/java/com/example/recipetracker/ui/theme/home/HomeScreen.kt
@@ -1,17 +1,7 @@
 package com.example.recipetracker.ui.theme.home
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
@@ -24,7 +14,6 @@ import com.example.recipetracker.R
 import com.example.recipetracker.ui.theme.components.RecipeCard
 import com.example.recipetracker.ui.theme.components.TopBar
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(navController: NavHostController) {
         LazyColumn(
